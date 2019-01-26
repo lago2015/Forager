15c46d116c5747b5329c663adcd24b7913c24be5
matthew-methot <matthewmethot@yahoo.ca>
2019-01-25 20:41:57 -0500

    adding project
    adding project

---
+++ Forager/Assets/Code/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour 
+{
+	int speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void Move(int direction)
+	{
+		
+	}
+}
