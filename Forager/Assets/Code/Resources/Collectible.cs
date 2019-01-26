df219c290e54f84b971bf1eb74cc476c6f05b1ab
lagoWizard <chilenoo92@gmail.com>
2019-01-26 12:40:42 -0500

    Material Resources
    - Player can interact with material resources that will be found inside asteroid shells
    - System will keep track of which material is collected
    - When player returns to hub the material resources are reset and calculated into the score

---
+++ Forager/Assets/Code/Resources/Collectible.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+
+    public enum MaterialSources { orb,smallAsteroid,medAsteroid,larAsteroid}
+    public MaterialSources currentMaterial;
+
+    private Player playerScript;
+	private void OnTriggerEnter(Collider other)
+	{
+        playerScript = other.GetComponent<Player>();
+        if(playerScript)
+        {
+            //add item to users inventory
+            playerScript.addToInventory(1);
+            playerScript.addMaterial(currentMaterial);
+            Destroy(gameObject);
+        }
+		
+	}
+}
