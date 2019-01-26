3b1d92fddaef6fdcf5d5dff69a246b0a92bda108
matthew-methot <matthewmethot@yahoo.ca>
2019-01-26 12:18:44 -0500

    Adding Arrow Stuff
    Adding arrow art and code

---
+++ Forager/Assets/Code/ArrowUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowUI : MonoBehaviour 
+{
+	public int rotationSpeed;
+	public Transform player;
+	public Transform home;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 dir = home.transform.position - player.transform.position;
+		
+		float angle = Mathf.Atan2(dir.x, dir.z)*Mathf.Rad2Deg;
+		
+		transform.eulerAngles = new Vector3(0, 0, angle * -1);
+	}
+}
