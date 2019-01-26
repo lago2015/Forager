59b3f4d7e5040bb70e9c36e4274646e17a6a0b6a
lagoWizard <chilenoo92@gmail.com>
2019-01-26 17:19:41 -0500

    Attempt at fixing arrow ui

--- Forager/Assets/Code/ArrowUI.cs
+++ Forager/Assets/Code/ArrowUI.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class ArrowUI : MonoBehaviour 
 {
-	public int rotationSpeed;
+	
 	public Transform player;
 	public Transform home;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
 		Vector3 dir = home.transform.position - player.transform.position;
 		
-		float angle = Mathf.Atan2(dir.x, dir.z)*Mathf.Rad2Deg;
+		float angle = Mathf.Atan2(dir.x, dir.y)*Mathf.Rad2Deg;
 		
 		transform.eulerAngles = new Vector3(0, 0, angle * -1);
 	}
