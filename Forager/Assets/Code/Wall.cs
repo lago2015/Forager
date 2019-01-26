7859e4843cd9661351cc5c31483e0734d5d7b355
matthew-methot <matthewmethot@yahoo.ca>
2019-01-26 03:58:04 -0500

    Add Wall Script

---
+++ Forager/Assets/Code/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		//we talked about doing something when the user collides with a wall
+	}
+	
+}
