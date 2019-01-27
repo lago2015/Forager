bdd5eb605c1165c1778937a8c51b27e35676c469
matthew-methot <matthewmethot@yahoo.ca>
2019-01-27 17:05:20 -0500

    Adding Game Manager
    Adding Game Manager

---
+++ Forager/Assets/Code/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	int enemyChaseSpeed = 50;
+	int numberOfEnemies = 21;
+	int numberOfColectables = 6;
+	int innerRingdistanceFromBase = 200;
+	int middleRingdistanceFromBase = 400;
+	int outerRingdistanceFromBase = 600;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void SpawnEnemies()
+	{
+		
+	}
+	void placeCollectibles()
+	{
+		//int random
+	}
+	void IncreaseDifficulty()
+	{
+		
+	}
+}
